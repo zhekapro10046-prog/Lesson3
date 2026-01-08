494f434d78f9cf63dad50d3715734436fe25ac99
Evgeny35 <zheka.pro10046@gmail.com>
2026-01-08 16:19:02 +0300

    commit

---
+++ Lesson7/index.js
@@ -0,0 +1,39 @@
+// const path = require ("node:path");
+
+// const fullPath = path.join(__dirname, "index.html");
+
+// console.log (fullPath);
+
+
+const path = require("node:path");
+const http = require("node:http");
+const fs = require("node:fs/promises");
+
+const PORT = 3000;
+
+
+const sendFile = async (fileName, response, statusCode) => {
+    const fullPath = path.join(__dirname, "public", fileName);
+    const page = await fs.readFile(fullPath, 'utf-8');
+    response.statusCode = statusCode; 
+    response.end(page);
+}
+
+const server = http.createServer(async (request, response) => {
+ response.setHeader ("Content-Type", "text/html; charset=utf-8");
+ 
+ if (request.url === '/') {
+    await sendFile ("index.html",response, 200);
+ } else if (request.url === '/about') {
+    await sendFile ("about.html",response, 200);
+ } else {
+    await sendFile ("404.html",response, 404);
+
+    
+ }
+ 
+});
+
+server.listen(PORT);
+console.log (`Сервер был успешно запущен по адресу http://localhost:${PORT}`);
+

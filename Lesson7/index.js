3551ad4f67ea3fd08e7dafabf18b811569134bcf
Evgeny35 <zheka.pro10046@gmail.com>
2026-01-09 18:59:34 +0300

    Homework

--- Lesson7/index.js
+++ Lesson7/index.js
@@ -5,6 +5,7 @@
 // console.log (fullPath);
 
 
+
 const path = require("node:path");
 const http = require("node:http");
 const fs = require("node:fs/promises");
@@ -14,6 +15,8 @@ const PORT = 3000;
 
 const sendFile = async (fileName, response, statusCode) => {
     const fullPath = path.join(__dirname, "public", fileName);
+ 
+   
     const page = await fs.readFile(fullPath, 'utf-8');
     response.statusCode = statusCode; 
     response.end(page);
@@ -24,12 +27,10 @@ const server = http.createServer(async (request, response) => {
  
  if (request.url === '/') {
     await sendFile ("index.html",response, 200);
- } else if (request.url === '/about') {
-    await sendFile ("about.html",response, 200);
+ } else if (request.url === '/rules') {
+    await sendFile ("rules.html",response, 200);
  } else {
     await sendFile ("404.html",response, 404);
-
-    
  }
  
 });
